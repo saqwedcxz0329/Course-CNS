d566f7f4c91ec46c6c9f7df4d6ecf699de23375e
saqwedcxz0329 <saqwedcxz0329@gmail.com>
2017-03-16 21:33:52 +0800

    pass round 1

--- hw1/src/ClassicalCipher.py
+++ hw1/src/ClassicalCipher.py
@@ -1,6 +1,30 @@
 from pwn import *
 
+lower_alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] 
+capital_alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
+            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+def decode(cipher, offset):
+    decode_msg = ""
+    for character in cipher:
+        ascii_code = ord(character)
+        append_character = ""
+        if (ascii_code == 32):
+            append_character = " "
+        # lower case
+        elif (ascii_code > 96):
+            ascii_code = ascii_code - 97
+            append_character = lower_alphabet[(ascii_code - offset) % 26]
+        # capital case
+        else:
+            ascii_code = ascii_code - 65
+            append_character = capital_alphabet[(ascii_code - offset) % 26]
+        decode_msg += append_character
+    return decode_msg
+
 r = remote("140.112.31.109", 10000)
+## Round 1
 r.recv()
 msg = r.recv()
 print msg
@@ -8,9 +32,19 @@ msg = msg.split("\n")
 m1 = msg[0].split("=")[1][1:]
 c1 = msg[1].split("=")[1][1:]
 c2 = msg[3].split("=")[1][1:]
-print m1[0]
-print c1[0]
-print abs(ord(m1[0]) - ord(c1[0])) % 26
-print c2
-#r.sendline("121233")
-#print r.recv()
+offset = ord(c1[0]) - ord(m1[0])
+m2 = decode(c2, offset)
+
+print m2 + "\n"
+r.sendline(m2)
+
+## Round 2
+r.recv()
+msg = r.recv()
+print msg
+msg = msg.split("\n")
+c1 = msg[0].split("=")[1][1:]
+m1 = decode(c1, offset)
+print m1 + "\n"
+r.sendline(m1)
+print r.recv()
