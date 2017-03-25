f3c3549ed50847092b4889d4842911cdedef750c
saqwedcxz0329 <saqwedcxz0329@gmail.com>
2017-03-25 23:30:01 +0800

    upgrade

--- hw1/MTP/code6.py
+++ hw1/MTP/code6.py
@@ -17,7 +17,9 @@ def strxor(a, b):     # xor two strings (trims the longer input)
 file = open("xor.txt", 'w')
 with open("cipher") as f:
     content = f.readlines()
+
 content = [x.strip() for x in content] 
+plaintext_list = [[None]*len(ciphertext) for ciphertext in content]
 
 for cur_index, ciphertext in enumerate(content):
     counter = collections.Counter()
@@ -31,13 +33,33 @@ for cur_index, ciphertext in enumerate(content):
             for char_index , character in enumerate(strxor(ciphertext.decode('hex'), ciphertext2.decode('hex'))):
                 if character != UNKNOWN_CHARACTER:
                     counter[char_index] += 1
+    space_index = []
     for index, value in counter.items():
         if value == 9:
-            key[index] = chr(ord(ciphertext.decode("hex")[index]) ^ ord(" "))
-    file.write("==============================\n")
-print key
-key_hex = ''.join([value.encode("hex") if value is not None else '00' for value in key])
-print key_hex
-for ciphertext in content:
-    print strxor(ciphertext.decode("hex"), key_hex.decode("hex"))
+            space_index.append(index)
+            
+    for index in space_index:
+        plaintext = plaintext_list[cur_index]
+        plaintext[index] = ' '
+        plaintext_list[cur_index] = plaintext
+        for c2_index, ciphertext in enumerate(content):
+            if cur_index != c2_index:
+                c1XORc2 = strxor(ciphertext.decode('hex'), ciphertext2.decode('hex'))
+                if c1XORc2[index] != "$" and c1XORc2[index]:
+                    plaintext2 = plaintext_list[c2_index]
+                    if c1XORc2[index].isupper():
+                        plaintext2[index] = c1XORc2[index].lower()
+                    else:
+                        plaintext2[index] = c1XORc2[index].upper()
+                    plaintext_list[c2_index] = plaintext2
+
+for plaintext in plaintext_list:
+    print plaintext
+#         key[index] = chr(ord(ciphertext.decode("hex")[index]) ^ ord(" "))
+#     file.write("==============================\n")
+# print key
+# key_hex = ''.join([value.encode("hex") if value is not None else '00' for value in key])
+# print key_hex
+# for ciphertext in content:
+#     print strxor(ciphertext.decode("hex"), key_hex.decode("hex"))
 file.close()
