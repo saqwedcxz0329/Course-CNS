d0ddc33ec34c66f67187424d6dba375ae6c77571
saqwedcxz0329 <saqwedcxz0329@gmail.com>
2017-04-05 18:32:57 +0800

    crack Q8

--- hw1/MITM/crack.py
+++ hw1/MITM/crack.py
@@ -8,51 +8,51 @@ p = 2626034878161944881812583523269882322103765919961462525429196058788050054696
 file = open("try.txt", "w")
 
 def brute(password):
-    try:
-        r = remote("140.112.31.109", 10002)
-        # password = [2, 20, 5]
-        key = 0
-        for pwd in password:
-            g = pow(pwd, 2, p)
-            b = 5
-            
-            # Receive   
-            recv = r.recv(timeout = 5)        # Round i
-            if recv == '':
-                raise Exception("Time out!!!")
-            A = r.recv(timeout = 5)    # Server send: ...
-            if A == '':
-                raise Exception("Time out!!!")
-            # print A
-            A = int(A[14:].split("G")[0][:-1])
-            assert(514 <= A < p-514)
-            K = pow(A, b, p)
-            key ^= int(hashlib.sha512(str(K)).hexdigest(), 16)
-            # Send
-            B = pow(g, b ,p)
-            r.sendline(str(B))
-
-        recv = r.recv(timeout = 5)
+    # try:
+    r = remote("140.112.31.109", 10002)
+    # password = [2, 20, 5]
+    key = 0
+    for pwd in password:
+        g = pow(pwd, 2, p)
+        b = 5
+        # Receive   
+        recv = r.recv(timeout = 5)        # Round i
         if recv == '':
-            raise Exception("Time out!!!!")
-        FLAG = int(recv[9:-1])
+            raise Exception("Time out!!!")
+        A = r.recv(timeout = 5)    # Server send: ...
+        if A == '':
+            raise Exception("Time out!!!")
+        A = int(A[14:].split("G")[0][:-1])
+        assert(514 <= A < p-514)
+        K = pow(A, b, p)
+        key ^= int(hashlib.sha512(str(K)).hexdigest(), 16)
+        # Send
+        B = pow(g, b ,p)
+        r.sendline(str(B))
 
-        file.write(str(password) + "\n")
-        file.write(str(hex(FLAG ^ key)[2:].decode("hex")) + "\n")
-        file.write("================\n")
-        print password
-        print hex(FLAG ^ key)[2:].decode("hex")
-        print "=========\n"
-        r.close()
-    except:
-        print "Error!!! try again."
-        r.close()
-        brute(password)
+    recv = r.recv(timeout = 5)
+    if recv == '':
+        raise Exception("Time out!!!!")
+    FLAG = int(recv[9:-1])
 
-for i in range(2, 21):
-    for j in range(2, 21):
-        for k in range(2, 21):
-            password = [i, j, k]
-            brute(password)
+    file.write(str(password) + "\n")
+    file.write(str(hex(FLAG ^ key)[2:].decode("hex")) + "\n")
+    file.write("================\n")
+    print password
+    print hex(FLAG ^ key)[2:].decode("hex")
+    print "=========\n"
+    r.close()
+    # except:
+    #     print "Error!!! try again."
+    #     r.close()
+    #     brute(password)
+
+password = [13, 19, 17]
+# for i in range(2, 21):
+#     for j in range(2, 21):
+#         for k in range(2, 21):
+password = [int(hashlib.sha512(str(pwd)).hexdigest(), 16) for pwd in password]
+# password = [i, j, k]
+brute(password)
 
 file.close()
