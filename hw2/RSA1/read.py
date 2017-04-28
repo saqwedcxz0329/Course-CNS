2206512b76b6132f8542e090ba4fba4e96bee054
saqwedcxz0329 <saqwedcxz0329@gmail.com>
2017-04-28 20:57:58 +0800

    upgrade rsa1

--- hw2/RSA1/read.py
+++ hw2/RSA1/read.py
@@ -1,19 +1,32 @@
 from Crypto.PublicKey import RSA
 import fractions
 
-def phi(n):
-    amount = 0
-    k = 1
-    while k < n + 1:
-        # print k
-        if fractions.gcd(n, k) == 1:
-            amount += 1
-        k += 1
-    return amount
-
+p = 1122830395489501760925799210383299
+q = 1232179875868643257203600345344573
+r = (p-1) * (q-1)
 kt = open("public.pem").read()
 key = RSA.importKey(kt)
-print key.exportKey()
-print "n: " +  str(key.n)
-print "e: " +  str(key.e)
-# print phi(key.n)
+n = key.n
+e = key.e
+print "n: " +  str(n)
+print "e: " +  str(e)
+print key
+d = '43dd1de2b05639352cd9a663b39389abbdbd0dcba43771badfe2149'
+d = int(d, 16)
+print 'd: ' + str(d)
+
+cipher = open('flag.enc').read()
+# print cipher
+cipher =  cipher.encode('hex')
+cipher = int(cipher, 16)
+print (d*e) % r
+# plaintext = pow(cipher, d, n)
+# print (hex(plaintext)[2:-1]).decode('hex')
+
+# msg = "abc"
+# msg = msg.encode('hex')
+# msg = int(msg, 16)
+# print msg
+# code = pow(msg, e, n)
+# plaintext = pow(code, d, n)
+# print (hex(plaintext)[2:-1]).decode('hex')
