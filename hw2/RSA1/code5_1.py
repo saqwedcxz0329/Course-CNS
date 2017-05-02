afe07523c6a375b554756c5f39f97f1cb30dcfcd
saqwedcxz0329 <saqwedcxz0329@gmail.com>
2017-05-03 02:29:21 +0800

    upgrade

--- hw2/RSA1/code5_1.py
+++ hw2/RSA1/code5_1.py
@@ -1,5 +1,12 @@
 from Crypto.PublicKey import RSA
 import fractions
+try:
+    import gmpy
+except ImportError as e:
+    try:
+        import gmpy2 as gmpy
+    except ImportError:
+        raise e
 
 p = 1122830395489501760925799210383299
 q = 1232179875868643257203600345344573
@@ -8,7 +15,8 @@ kt = open("public.pem").read()
 key = RSA.importKey(kt)
 n = key.n
 e = key.e
-d = 446680355796085616328464905309110344309206844273229453296104513865
-private_key = RSA.construct((n, e, d, p, q))
+d = gmpy.invert(e, r)
+# d = 446680355796085616328464905309110344309206844273229453296104513865
+private_key = RSA.construct((n, e, long(d), p, q))
 dsmg = private_key.decrypt(open('flag.enc').read())
 print dsmg
