7d4617105fc1e517a65e3c61d84c702ae87055c3
saqwedcxz0329 <saqwedcxz0329@gmail.com>
2017-06-18 15:46:38 +0800

    update

--- hw3/Dos_On_Hash_Table/dos.cpp
+++ hw3/Dos_On_Hash_Table/dos.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 #include <unordered_map>
+#include <fstream>
 using namespace std;
 int main() {
     unordered_map<int, int> ht;
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        int x;
-        cin >> x;
-        ht[x] = i;
+    string line;
+    ifstream inputFile;
+    inputFile.open("./input6-1.txt");
+    if (inputFile.is_open())
+    {   
+        int value = 0;
+        getline (inputFile,line);
+        while ( getline (inputFile,line) )
+        {   
+            cout << value << '\n';
+            int index = stoi(line);
+            ht[index] = value;
+            value++;
+        }
+        inputFile.close();
     }
+
+    cout << "Bucket number: " << ht.bucket_count() << endl;
     return 0;
 }
